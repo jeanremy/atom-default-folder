0be86bd3945454978744be229ba1df6a77fd5805
JR <jr@MacBook-Pro-de-JR.local>
2017-05-10 18:17:30 +0200

    first commit

---
+++ spec/atom-default-folder-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import AtomDefaultFolderView from '../lib/atom-default-folder-view';
+
+describe('AtomDefaultFolderView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
